d1567eed257ae9e05a217e3f9a2dce9e87e4053d
MariaJimenez10 <mariapaulajime10@gmail.com>
2024-11-25 11:48:16 -0500

    Se sube la tienda Online

--- index.php
+++ index.php
@@ -1,79 +1,200 @@
-<?php
-  if (!empty($_GET['q'])) {
-    switch ($_GET['q']) {
-      case 'info':
-        phpinfo(); 
-        exit;
-      break;
-    }
-  }
-?>
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Laragon</title>
+<html lang="en-US">
 
-        <link href="https://fonts.googleapis.com/css?family=Karla:400" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Karla';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-
-            .opt {
-                margin-top: 30px;
-            }
-
-            .opt a {
-              text-decoration: none;
-              font-size: 150%;
-            }
-            
-            a:hover {
-              color: red;
-            }
-        </style>
-    </head>
-    <body>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name='robots' content='noindex,nofollow' />
+    <title>Molduras Sinú</title>
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Roboto:wght@400;700&display=swap">
+    <link href="stylesindex.css" rel="stylesheet" />
+    <script type="text/javascript"
+        src="https://149842022.v2.pressablecdn.com/illdy/wp-includes/js/jquery/jquery.min.js?ver=3.7.1"
+        id="jquery-core-js"></script>
+    <script type="text/javascript"
+        src="https://149842022.v2.pressablecdn.com/illdy/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.4.1"
+        id="jquery-migrate-js"></script>
+    <script type="text/javascript" id="illdy-pace-js-extra">
+        var paceOptions = { "restartOnRequestAfter": "0", "restartOnPushState": "0" };
+    </script>
+    <script type="text/javascript"
+        src="https://149842022.v2.pressablecdn.com/illdy/wp-content/themes/illdy/layout/js/pace/pace.min.js?ver=6.6.2"
+        id="illdy-pace-js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+<header id="header" class="header-front-page" style="position: relative; z-index: 2;">
+    <div class="top-header"
+        style="background: rgba(0, 0, 0, 0.7); backdrop-filter: blur(10px); -webkit-backdrop-filter: blur(10px); padding: 20px 0;">
         <div class="container">
-            <div class="content">
-                <div class="title" title="Laragon">Laragon</div>
-     
-                <div class="info"><br />
-                      <?php print($_SERVER['SERVER_SOFTWARE']); ?><br />
-                      PHP version: <?php print phpversion(); ?>   <span><a title="phpinfo()" href="/?q=info">info</a></span><br />
-                      Document Root: <?php print ($_SERVER['DOCUMENT_ROOT']); ?><br />
-
+            <div class="row">
+                <div class="col-sm-4 col-xs-8">
+                    <a href="index.php" title="Molduras Sinu">
+                        <img class="header-logo-img" src="images/Logos/logo.png" width="150" alt="Logo Molduras Sinú" />
+                    </a>
                 </div>
-                <div class="opt">
-                  <div><a title="Getting Started" href="https://laragon.org/docs">Getting Started</a></div>
+                <div class="col-sm-8 col-xs-4">
+                    <nav class="header-navigation">
+                        <ul id="menu-illdy-main" class="clearfix">
+                            <li id="menu-item-16" class="menu-item">
+                                <a href="index.php">Inicio</a>
+                            </li>
+                            <li id="menu-item-20" class="menu-item">
+                                <a href="ubicacion.html">Ubicación</a>
+                            </li>
+                            <li id="menu-item-22" class="menu-item">
+                                <a href="contactanos.html">Contactanos</a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <button class="open-responsive-menu"><i class="fa fa-bars"></i></button>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- Contenedor para el video -->
+<div class="content" style="position: relative; z-index: 1;">
+    <div class="video-container" style="position: relative; height: 80vh; overflow: hidden;"> <!-- Usando vh -->
+        <video autoplay loop muted playsinline
+            style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover;">
+            <source src="images/Talleres/fondof.mp4" type="video/mp4">
+        </video>
+        <div class="bottom-header front-page">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h1>Los mejores productos <span class="span-dot"></span>los encuentras<span class="span-dot">
+                            </span>en
+                            Molduras Sinú</h1>
+                    </div><!--/.col-sm-12-->
+                    <div class="col-sm-8 col-sm-offset-2">
+                        <div class="section-description">Marqueteria , Ebanisteria , Decoración</div>
+                        <div class="button-container">
+                            <a href="https://www.whatsapp.com/catalog/573107252567/?app_absent=0" title="Learn more"
+                                class="header-button-one">Whatsapp</a>
+                            <a href="productos.html" title="Download" class="header-button-two">Compra Ahora</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </br>
+    </div>
+</div>
+<br>
+<section id="about" class="front-page-section" style="margin-bottom: 0; padding: 0;">
+    <div class="container" style="padding: 0;">
+        <div class="row">
+            <div class="col-sm-12" style="padding: 0;">
+                <h3 style="color: black;">Sobre Nosotros</h3>
+            </div>
+            <br>
+            <br>
+            <br>
+        </div>
+    </div>
+    </div><!--/.col-sm-12-->
+    <div class="col-sm-10 col-sm-offset-1" style="padding: 0;">
+        <div class="section-description" style="color: black;">
+            Molduras El Sinú es una empresa dedicada a la marquetería, ebanistería y decoración, con más de 20 años
+            de experiencia en el sector. Durante nuestra trayectoria, hemos perfeccionado el arte de trabajar la
+            madera y sus derivados, ofreciendo productos de alta calidad y soluciones personalizadas para nuestros
+            clientes.<br>
+            Nos enorgullecemos de ofrecer productos duraderos, estéticos y funcionales, combinando tradición y
+            modernidad para embellecer tus espacios y crear muebles únicos que reflejan el estilo y gusto de cada
+            cliente.
+        </div>
+    </div>
+</section>
+<!-- Sección de la Sede -->
+<section id="projects" class="front-page-section py-5 bg-light">
+    <div class="container text-center">
+        <a href="materiales.html" class="text-decoration-none text-black">
+            <h3 style="color: black;">Nuestra Segunda Sede</h3>
+        </a>
+        <p class="section-description" style="color:black">Km 13 Via Cali-Popayán</p>
+        <div class="mt-4">
+            <img src="images/Logos/aserradero.jpg" alt="Imagen de Nuestra Segunda Sede" class="img-fluid rounded"
+                style="width: 100%; max-width: 1200px; height: auto; object-fit: cover;">
+        </div>
+    </div>
+</section>
+
+<!-- Separador -->
+<div class="my-5"></div>
+
+<!-- Sección de Servicios -->
+<!-- Sección de Servicios -->
+<section id="services" class="front-page-section py-5">
+    <div class="container">
+        <div class="text-center mb-5">
+            <h3 class="fw-bold">
+                <i class="fa-solid fa-wrench text-warning me-2"></i><span style="color: black;"><br>Servicios</span>
+            </h3>
+            <p class="section-description" style="color: black;">En Molduras El Sinú ofrecemos servicios de:</p>
+        </div>
+        <br>
+
+        <!-- Servicios -->
+        <div class="row g-4 justify-content-center">
+            <!-- Servicio 1 -->
+            <div class="col-md-4">
+                <div class="card border-0 text-center shadow h-100">
+                    <div class="card-body">
+                        <i class="fa-solid fa-paint-roller" style="color: #fcb900; font-size: 40px;"></i>
+                        <h5 class="card-title mt-3">
+                            <a href="Taller pintura.html" style="color: black;">Taller de Pintura</a>
+                        </h5>
+                        <br>
+                        <p style="color: black;">"En nuestro taller de pintura, cada trazo es una oportunidad para
+                            liberar la creatividad y transformar ideas en obras de arte."</p>
+                        </p>
+                    </div>
                 </div>
             </div>
 
+            <!-- Servicio 2 -->
+            <div class="col-md-4">
+                <div class="card border-0 text-center shadow h-100">
+                    <div class="card-body">
+                        <i class="fa fa-couch" style="color: #8B4513; font-size: 40px;"></i>
+                        <h5 class="card-title mt-3">
+                            <a href="muebles.html" style="color: black;">Muebles</a>
+                        </h5>
+                        <br>
+                        <p class="card-text" style="color: black;">
+                            "La calidad y creatividad se unen para dar vida a espacios acogedores con muebles únicos."
+                        </p>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Servicio 3 -->
+            <div class="col-md-4">
+                <div class="card border-0 text-center shadow h-100">
+                    <div class="card-body">
+                        <i class="fa fa-tools" style="color: #0693e3; font-size: 40px;"></i>
+                        <h5 class="card-title mt-3">
+                            <a href="materiales.html" style="color: black;">Molduras Para Construcción</a>
+                        </h5>
+                        <br>
+                        <p class="card-text" style="color: black;">
+                            "Nuestras molduras ofrecen elegancia y durabilidad, realzando cada espacio."
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
-    </body>
+    </div>
+</section>
+<div class="bottom-footer">
+    <div class="container">
+        <p class="copyright">
+            <span class="bottom-copyright" data-customizer="copyright-credit">© Molduras Sinú 2024. Todos los derechos
+                reservados</span>
+        </p>
+    </div>
+</div>
 </html>
